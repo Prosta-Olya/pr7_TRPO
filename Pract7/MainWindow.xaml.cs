aaedc160ca73707348fe0b212bf8a176e232ee06
Olya <siverolya@gmail.com>
2025-10-27 17:17:26 +0500

    commit

---
+++ Pract7/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Pract7.MainWindow;
+
+namespace Pract7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User CurrentUser = new User();
+        private string folder = "Users";
+        public MainWindow()
+        {
+            InitializeComponent();
+            panel1.DataContext = CurrentUser;
+            panel3.DataContext = CurrentUser;
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            RandomId();
+        }
+        private void RandomId()
+        {
+            Random random = new Random();
+            bool idExists;
+            string id;
+            do
+            {
+                id = random.Next(1000, 100000).ToString();
+                string filePath = System.IO.Path.Combine(folder, $"D_{id}.json");
+                idExists = File.Exists(filePath);
+            }while(idExists);
+            CurrentUser.Id = id;
+        }
+        private void Registr(object sender, RoutedEventArgs e)
+        {
+            if (CurrentUser.Password != TextBoxPassword.Text)
+            {
+                MessageBox.Show("Пароли не совпадают!");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(CurrentUser.Name) || string.IsNullOrWhiteSpace(CurrentUser.LastName) || string.IsNullOrWhiteSpace(CurrentUser.MiddleName) || string.IsNullOrWhiteSpace(CurrentUser.Specialisation) || string.IsNullOrWhiteSpace(CurrentUser.Password))
+            {
+                MessageBox.Show("Заполните все поля для регистрации!");
+                return;
+            }
+            var user = new
+            {
+                Id = CurrentUser.Id,
+                Name = CurrentUser.Name,
+                LastName = CurrentUser.LastName,
+                MiddleName = CurrentUser.MiddleName,
+                Specialisation = CurrentUser.Specialisation,
+                Password = CurrentUser.Password
+            };
+            string json = JsonSerializer.Serialize(user);
+            string filePath = System.IO.Path.Combine(folder, $"D_{CurrentUser.Id}.json");
+            IdTextBox.Text = CurrentUser.Id;
+            //string info = $"\"Name\": \"{CurrentUser.Name}\",\r\n\"LastName\": \"{CurrentUser.LastName}\",\r\n\"MiddleName\": \"{CurrentUser.MiddleName}\",\r\n\"Specialisation\": \"{CurrentUser.Specialisation}\",\r\n\"Password\": \"{CurrentUser.Password}\"";
+            File.WriteAllText(filePath, json);
+            MessageBox.Show("Вы зарегистрированы!");
+        }
+
+        private void Input(object sender, RoutedEventArgs e)
+        {
+            string id = TextBoxId.Text;
+            string password = PasswordTextBox.Text;
+            if(string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Заполните все поля для входа!");
+                return;
+            }
+            string filePath = System.IO.Path.Combine(folder, $"D_{CurrentUser.Id}.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                var user = JsonSerializer.Deserialize<User>(json);
+                if (user.Password == password)
+                {
+                    CurrentUser.Id = user.Id;
+                    CurrentUser.Name = user.Name;
+                    CurrentUser.LastName = user.LastName;
+                    CurrentUser.MiddleName = user.MiddleName;
+                    CurrentUser.Specialisation = user.Specialisation;
+                    CurrentUser.Password = user.Password;
+                    MessageBox.Show("Вход выполнен успешно");
+                }
+                else
+                {
+                    MessageBox.Show("Неверный пароль");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не зарегистрирован");
+                return;
+            }
+        }
+    }
+}
---
+++ Pract7/User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class User : INotifyPropertyChanged
+{
+    private string _id = "00000";
+    public string Id
+    {
+        get => _id;
+        set
+        {
+            if (_id != value)
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _name = "Имя";
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != value)
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _lastName = "Фамилия";
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            if (_lastName != value)
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _middleName = "Отчество";
+    public string MiddleName
+    {
+        get => _middleName;
+        set
+        {
+            if (_middleName != value)
+            {
+                _middleName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _specialisation = "Специализация";
+    public string Specialisation
+    {
+        get => _specialisation;
+        set
+        {
+            if (_specialisation != value)
+            {
+                _specialisation = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _password = "Пароль";
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            if (_password != value)
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public User()
+    {
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? propName =
+    null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
